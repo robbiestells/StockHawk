f3a797f7d57afbb5da767c975a46cfd9099950a1
rsteller <robbiestells@hotmail.com>
2016-12-14 14:40:28 -0500

    trying to make widget

--- app/src/main/java/com/udacity/stockhawk/widget/widgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widget/widgetProvider.java
@@ -3,6 +3,7 @@ package com.udacity.stockhawk.widget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
@@ -10,6 +11,8 @@ import android.widget.RemoteViews;
 import android.widget.Toast;
 
 import com.udacity.stockhawk.R;
+import com.udacity.stockhawk.sync.QuoteSyncJob;
+import com.udacity.stockhawk.ui.MainActivity;
 
 /**
  * Created by rsteller on 12/14/2016.
@@ -20,11 +23,29 @@ public class WidgetProvider extends AppWidgetProvider {
     @Override
     public void onReceive(Context context, Intent intent) {
         super.onReceive(context, intent);
+        if (QuoteSyncJob.ACTION_DATA_UPDATED.equals(intent.getAction())) {
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(
+                    new ComponentName(context, getClass()));
+            appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.widgetCollectionList);
+        }
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            RemoteViews views = new RemoteViews(
+                    context.getPackageName(),
+                    R.layout.widget_provider_layout);
+
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context,
+                    0, intent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+
+        }
     }
 }
